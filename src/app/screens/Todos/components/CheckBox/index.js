0e19e8b8033ab4162c7eaadafde3807983832583
M4teCocido <rodrigohernandez6684@gmail.com>
2018-11-24 13:06:13 -0300

    checkbox

---
+++ src/app/screens/Todos/components/CheckBox/index.js
@@ -0,0 +1,22 @@
+import React, { Component, PropTypes } from 'react'
+import { View, StyleSheet, TouchableOpacity } from 'react-native'
+
+export default class Checkbox extends Component {
+
+  static propTypes = {
+    onToggle: PropTypes.func,
+    isChecked: PropTypes.bool,
+  }
+
+  render() {
+    const {onToggle, isChecked} = this.props
+
+    return (
+      <TouchableOpacity onPress={onToggle}>
+        <View style={styles.box}>
+          { isChecked && <View style={styles.inner}/> }
+        </View>
+      </TouchableOpacity>
+    )
+  }
+}
