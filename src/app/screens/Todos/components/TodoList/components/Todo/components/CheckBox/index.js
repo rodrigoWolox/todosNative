85f56917d992ae165e1d291b3e1d3f33775db243
M4teCocido <rodrigohernandez6684@gmail.com>
2018-11-24 20:48:45 -0300

    checkbox done

--- src/app/screens/Todos/components/TodoList/components/Todo/components/CheckBox/index.js
+++ src/app/screens/Todos/components/TodoList/components/Todo/components/CheckBox/index.js
@@ -1,29 +1,19 @@
-import React, { Component, PropTypes } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 import { View, StyleSheet, TouchableOpacity } from 'react-native'
 import styles from './styles';
 
+const Checkbox = ({ onToggle, isChecked }) => (
+  <TouchableOpacity onPress={this.props.onToggle}>
+    <View style={styles.box}>
+      { this.props.isChecked && <View style={styles.inner} /> }
+    </View>
+  </TouchableOpacity>
+);
 
-export default class Checkbox extends Component {
-
-  static propTypes = {
-    onToggle: PropTypes.func,
-    isChecked: PropTypes.bool,
-  }
-
-  render() {
-    const {onToggle, isChecked} = this.props
-
-    return (
-      <TouchableOpacity onPress={onToggle}>
-        <View style={styles.box}>
-          { isChecked && <View style={styles.inner}/> }
-        </View>
-      </TouchableOpacity>
-    )
-  }
+CheckBox.propTypes = {
+  onToggle: PropTypes.func,
+  isChecked: PropTypes.bool
 }
-
-
-
 
 export default CheckBox;
--- src/app/screens/Todos/components/TodoList/components/Todo/index.js
+++ src/app/screens/Todos/components/TodoList/components/Todo/index.js
@@ -4,25 +4,31 @@ import PropTypes from 'prop-types';
 import Checkbox from './components/Checkbox';
 
 class Todo extends React.Component {
-  toggle = () => onToggle(this.props.key)
-
+  remove = () => this.props.onRemove(this.props.key);
 
 	render() {
 		return (
-			<View key={i} style={itemStyle}>
-        <Text> {item.label} </Text>
+			<View key={key} style={itemStyle}>
+        <Text>{this.props.todo.label}</Text>
         <View style={styles.rightSection}>
           <Checkbox
-            isChecked={item.completed}
-            onToggle={}
+            isChecked={this.props.todo.isChecked}
+            onToggle={this.props.onToggle}
           />
-          <TouchableOpacity onPress={() => onRemoveItem(i)}>
+          <TouchableOpacity onPress={this.remove}>
             <Text style={styles.remove}> &times; </Text>
           </TouchableOpacity>
         </View>
       </View>
 		)
 	}
+}
+
+Todo.propTypes = {
+  key: PropTypes.number.isRequired,
+  onRemove: PropTypes.func.isRequired,
+  onToggle: PropTypes.func.isReuired,
+  todo: PropTypes.object
 }
 
 export default Todo;
