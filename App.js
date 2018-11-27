40266461f7383238a8da905f70d784fb2c695b87
rodrigoWolox <rodrigo.hernandez@wolox.com.ar>
2018-11-27 16:30:29 -0300

    navigation installed

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-
 import { store } from './src/redux/store';
-import App from './src/app/screens/Todos';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
-export default class extends React.Component {
+import Todos from './src/app/screens/Todos';
+
+const AppNavigator = createStackNavigator({
+  Todos: Todos
+});
+
+class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <App />
+        <Todos />
       </Provider>
     )
   }
 }
+
+export default createAppContainer(App);
