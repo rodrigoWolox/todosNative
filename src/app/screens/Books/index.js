e42d6cfb5a835eba6384e2343f06595e4335dd6e
rodrigoWolox <rodrigo.hernandez@wolox.com.ar>
2018-11-28 14:16:19 -0300

    Book List and components added

--- src/app/screens/Books/index.js
+++ src/app/screens/Books/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View } from 'react-native';
+import BookList from './components/BookList';
 import { connect } from 'react-redux';
 import styles from './styles';
 
 const Books = () => (
   <View style={styles.container}>
-    <Text>Book List</Text>
+    <BookList 
+      books={}
+    />
   </View>
 );
 
 Books.propTypes = {
-  
+  books: PropTypes.arrayOf()
 };
 
 export default connect()(Books);
